daba39f6e4c7595074ad9bf4353fd51c27413def
muhammadaqilp <muhammadpratama1816@gmail.com>
2022-03-10 22:42:43 +0700

    add data dummy

--- core/src/main/java/com/example/core/utils/DataDummy.kt
+++ core/src/main/java/com/example/core/utils/DataDummy.kt
@@ -1,5 +1,8 @@
 package com.example.core.utils
 
+import com.example.core.data.source.local.entity.GamesEntity
+import com.example.core.data.source.remote.response.GamesResponse
+import com.example.core.data.source.remote.response.Genres
 import com.example.core.domain.model.Games
 
 object DataDummy {
@@ -16,6 +19,41 @@ object DataDummy {
                     "Lorem Ipsum $i",
                     "Adventure, Science",
                     false
+                )
+            )
+        }
+        return list
+    }
+
+    fun generateGameEntities(): List<GamesEntity> {
+        val list = mutableListOf<GamesEntity>()
+        for (i in 0..9) {
+            list.add(
+                GamesEntity(
+                    i,
+                    "Games $i",
+                    "Image $i",
+                    i.toDouble(),
+                    "Lorem Ipsum $i",
+                    "Adventure, Science",
+                    false
+                )
+            )
+        }
+        return list
+    }
+
+    fun generateGameResponse(): List<GamesResponse> {
+        val list = mutableListOf<GamesResponse>()
+        for (i in 0..9) {
+            list.add(
+                GamesResponse(
+                    i,
+                    "Games $i",
+                    "Image $i",
+                    i.toDouble(),
+                    "Lorem Ipsum $i",
+                    listOf(Genres(name = "Adventure", id = 1)),
                 )
             )
         }
