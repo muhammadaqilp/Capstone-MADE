8080dcaca48fa20ea1d4cfb17d618963c4ef4099
muhammadaqilp <muhammadpratama1816@gmail.com>
2022-03-10 22:43:30 +0700

    minor changes in Resource.kt

--- core/src/main/java/com/example/core/data/Resource.kt
+++ core/src/main/java/com/example/core/data/Resource.kt
@@ -1,6 +1,6 @@
 package com.example.core.data
 
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+sealed class Resource<T>(var data: T? = null, val message: String? = null) {
     class Success<T>(data: T) : Resource<T>(data)
     class Loading<T>(data: T? = null) : Resource<T>(data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data)
